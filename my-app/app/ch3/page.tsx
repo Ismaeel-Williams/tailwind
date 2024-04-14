57e87498cf1309c2a55f160bbe585495b85a4988
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-04-14 21:41:03 +0200

    ch3

---
+++ my-app/app/ch3/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ch3 = () => {
+  return (
+    <div className='bg-[#23424A]'>
+      <div className='w-[70%] p-20'>
+        <div className='text-white'>
+            <h1 className='text-4xl mb-4 font-bold'>Responsive layouts don't have to be a struggle</h1>
+            <p className='text-lg mb-4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat nam dolor aut, similique vitae modi adipisci. Facilis beatae inventore sunt ipsa tempora? Dolores, corporis! Obcaecati.</p>
+        </div>
+        <button className='rounded-3xl bg-[#38cfd9] py-2 px-6 mt-4'>I WANT TO LEARN</button>
+      </div>
+    </div>
+  )
+}
+
+export default ch3
