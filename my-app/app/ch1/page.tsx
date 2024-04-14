3a0027dfaed40ac845ee05aa418778c9ee35bd4a
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-04-14 14:00:16 +0200

    ch2

---
+++ my-app/app/ch1/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ch1() {
+  return (
+    <div className="bg-[#23424a]">
+      <div className='py-10 px-10 text-white'>
+        <h1 className="text-4xl">Lorem ipsum dolor sit.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quas earum sapiente recusandae dolor rerum odio maiores tenetur deleniti aperiam!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quas earum sapiente recusandae dolor rerum odio maiores tenetur deleniti aperiam!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quas earum sapiente recusandae dolor rerum odio maiores tenetur deleniti aperiam!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quas earum sapiente recusandae dolor rerum odio maiores tenetur deleniti aperiam!</p>
+      </div>
+    </div>
+  )
+}
+
+export default ch1
---
+++ my-app/app/ch2/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ch2 = () => {
+  return (
+    <div>
+        <div className='bg-[#23424a] pl-60'>
+            <div className='text-white max-w-lg py-10'>
+                <h1 className='text-2xl' pb-6>Lorem ipsum dolor sit.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sint ipsum harum repudiandae ut assumenda ad voluptatum necessitatibus aut impedit!</p>
+            </div>
+        </div>
+        <div className='pl-60 space-y-2'>
+            <h2 className='font-bold text-xl mt-4'>more content D:</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, unde pariatur. Minus, quod quos doloribus fugiat iure vel debitis impedit?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quod vitae, error exercitationem omnis rerum mollitia, sunt autem, fugiat totam quo! Repellat dolorum blanditiis cupiditate.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima sed dolore veritatis. Quas voluptatem odio voluptate minima fuga, enim quis voluptatum qui illo! Deleniti eligendi impedit quasi. At, nisi deserunt!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores mollitia tempora vero. Perferendis, ea harum!</p>
+        </div>
+    </div>
+  )
+}
+
+export default ch2
