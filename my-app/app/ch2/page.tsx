6ae8e8beeb90fc20173d1794627825e5b0db83cd
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-04-14 14:10:03 +0200

    ch2

--- my-app/app/ch2/page.tsx
+++ my-app/app/ch2/page.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const ch2 = () => {
   return (
-    <div>
+    <div className='flex justify-center flex-col'>
         <div className='bg-[#23424a] pl-60'>
             <div className='text-white max-w-lg py-10'>
                 <h1 className='text-2xl' pb-6>Lorem ipsum dolor sit.</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sint ipsum harum repudiandae ut assumenda ad voluptatum necessitatibus aut impedit!</p>
             </div>
         </div>
-        <div className='pl-60 space-y-2'>
+        <div className='pl-60 space-y-2 max-w-6xl'>
             <h2 className='font-bold text-xl mt-4'>more content D:</h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt, unde pariatur. Minus, quod quos doloribus fugiat iure vel debitis impedit?</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quod vitae, error exercitationem omnis rerum mollitia, sunt autem, fugiat totam quo! Repellat dolorum blanditiis cupiditate.</p>
