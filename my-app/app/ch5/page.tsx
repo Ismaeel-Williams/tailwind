eef663cdb9941e0f259bba8849216475d2c5c31a
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-04-17 11:53:46 +0200

    ch5 html

---
+++ my-app/app/ch5/page.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const ch5 = () => {
+  return (
+    <body>
+        <section>
+            <div>
+                <div>
+
+                    <h1>Responsive layouts dont have to be a struggle</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio accusamus non fuga hic, voluptatum veritatis.</p>
+                    <button>I WANT TO LEARN</button>
+                </div>
+                <img></img>
+            </div>
+        </section>
+        <section>
+            <div>
+                <div>
+                    <h1>Quality designs made custom, on demand, just for you</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro recusandae praesentium libero alias non ullam facilis provident rem odio quam quod, modi perspiciatis? Reprehenderit quibusdam eveniet, ipsa sint omnis saepe!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores deleniti excepturi dolorem non placeat. Harum debitis illum eius quidem nesciunt laudantium accusamus, fugiat cupiditate cum id error odio rem?</p>
+                </div>
+                <div>
+                    <div>
+                        <h3>Cheap</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium accusamus accusantium quae animi error ipsum voluptate officia debitis porro.</p>                       
+                    </div>
+                    <div>
+                        <h3>Cheap</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium accusamus accusantium quae animi error ipsum voluptate officia debitis porro.</p>                       
+                    </div>
+                    <div>
+                        <h3>Cheap</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium accusamus accusantium quae animi error ipsum voluptate officia debitis porro.</p>                       
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+  )
+}
+
+export default ch5
