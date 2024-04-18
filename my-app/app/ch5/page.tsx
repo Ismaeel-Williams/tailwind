b93d54a20e6753f48c1bcd14a688fac6135cf7cc
Ismaeel <Ismaeelwilliams26@gmail.com>
2024-04-18 14:26:33 +0200

    ch5 fix

--- my-app/app/ch5/page.tsx
+++ my-app/app/ch5/page.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 const ch5 = () => {
   return (
     <body>
-        <section>
-            <div>
+        <section className='bg-[#23424A]'>
+            <div className='flex'>
                 <div>
-
-                    <h1>Responsive layouts dont have to be a struggle</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio accusamus non fuga hic, voluptatum veritatis.</p>
-                    <button>I WANT TO LEARN</button>
+                    <div className='text-white'>                     
+                        <h1 className='text-2xl font-bold'>Responsive layouts dont have to be a struggle</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio accusamus non fuga hic, voluptatum veritatis.</p>
+                    </div>
+                    <button className='rounded bg-[#38CFD9]'>I WANT TO LEARN</button>
                 </div>
                 <img></img>
             </div>
@@ -18,13 +19,13 @@ const ch5 = () => {
             <div>
                 <div>
                     <h1>Quality designs made custom, on demand, just for you</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro recusandae praesentium libero alias non ullam facilis provident rem odio quam quod, modi perspiciatis? Reprehenderit quibusdam eveniet, ipsa sint omnis saepe!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores deleniti excepturi dolorem non placeat. Harum debitis illum eius quidem nesciunt laudantium accusamus, fugiat cupiditate cum id error odio rem?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
                 </div>
                 <div>
                     <div>
                         <h3>Cheap</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod praesentium accusamus accusantium quae animi error ipsum voluptate officia debitis porro.</p>                       
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>                       
                     </div>
                     <div>
                         <h3>Cheap</h3>
